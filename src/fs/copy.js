5077ef9af0cc64fc40f729c1edd40f00a324d4e7
Yulya <yulya.vasilyeva@logicsoftware.net>
2023-06-12 22:58:35 +0200

    create function to copy repository

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const folderPath = __dirname + '/files';
+    const copyFolderPath = __dirname + '/files_copy';
+
+    const error = new Error('FS operation failed');
+
+    if (fs.existsSync(copyFolderPath)) {
+        throw error;
+    } else if (fs.existsSync(folderPath)) {
+        fs.cp(folderPath, copyFolderPath, { recursive: true }, (err) => {
+            if (err) throw error;
+        });
+    }
 };
 
 await copy();
