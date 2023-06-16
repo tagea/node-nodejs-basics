aa2a5c423129d1d5e7a4070861966334b0e73e66
Yulya <yulya.vasilyeva@logicsoftware.net>
2023-06-16 18:22:56 +0200

    add function for list of the files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import { getFolderName } from './get_path.js';
+
 const list = async () => {
-    // Write your code here 
+    const folder = getFolderName('files');
+
+    const error = new Error('FS operation failed');
+
+    if (!fs.existsSync(folder)) {
+        throw error;
+    } else {
+        fs.readdir(folder, (err, files) => {
+            if (err) throw error;
+            console.log(files);
+        });
+    }
 };
 
 await list();
