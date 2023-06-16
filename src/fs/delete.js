a5fd69cefc45f19e99ec946f7f09aead4e420071
Yulya <yulya.vasilyeva@logicsoftware.net>
2023-06-16 08:53:16 +0200

    remove file by name

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import { getFilePath } from './get_path.js';
+
 const remove = async () => {
-    // Write your code here 
+    const fileName = getFilePath('files', 'fileToRemove.txt');
+
+    const error = new Error('FS operation failed');
+
+    if (!fs.existsSync(fileName)) {
+        throw error;
+    } else {
+        fs.unlink(fileName, (err) => {
+            if (err) throw error;
+        });
+    }
 };
 
 await remove();
