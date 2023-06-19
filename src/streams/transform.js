4c2807703596a77460912b509331c82d449da01b
Yulya <yulya.vasilyeva@logicsoftware.net>
2023-06-19 20:45:51 +0200

    implement transform stream function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseData = new Transform({
+        transform(chunk, encoding, callback) {
+            const reverseChunk = chunk.toString().split('').reverse().join('');
+            this.push(reverseChunk);
+            callback();
+        },
+    });
+    process.stdin.pipe(reverseData).pipe(process.stdout);
 };
 
 await transform();
