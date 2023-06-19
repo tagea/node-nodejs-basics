175ceff1f01999857ab39dd6a3e829ac4c971ea7
Yulya <yulya.vasilyeva@logicsoftware.net>
2023-06-19 22:15:17 +0200

    implement zip and unzip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import { createGzip } from 'zlib';
+import { getFilePath } from '../utility/get_path.js';
+import { getErrors } from '../utility/errors.js';
+
 const compress = async () => {
-    // Write your code here 
+    const fileName = getFilePath('files', 'fileToCompress.txt', import.meta.url);
+    const fileNameGZ = getFilePath('files', 'archive.gz', import.meta.url);
+    const error = getErrors('file');
+
+    if (!fs.existsSync(fileName)) {
+        throw error;
+    } else {
+        const gzip = createGzip();
+        const source = fs.createReadStream(fileName, { encoding: 'utf8' });
+        const destination = fs.createWriteStream(fileNameGZ);
+        source.pipe(gzip).pipe(destination);
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import { createUnzip } from 'zlib';
+import { getFilePath } from '../utility/get_path.js';
+import { getErrors } from '../utility/errors.js';
+
 const decompress = async () => {
-    // Write your code here 
+    const fileName = getFilePath('files', 'fileToCompress.txt', import.meta.url);
+    const fileNameGZ = getFilePath('files', 'archive.gz', import.meta.url);
+    const error = getErrors('file');
+
+    if (!fs.existsSync(fileNameGZ)) {
+        throw error;
+    } else {
+        const unzip = createUnzip();
+        const source = fs.createReadStream(fileNameGZ);
+        const destination = fs.createWriteStream(fileName);
+        source.pipe(unzip).pipe(destination);
+    }
 };
 
 await decompress();
