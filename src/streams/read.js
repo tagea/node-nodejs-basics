1d03bbafc45bb889b102aa91324536a30131630f
Yulya <yulya.vasilyeva@logicsoftware.net>
2023-06-19 17:01:16 +0200

    implement readable stream function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
+import * as fs from 'fs';
+import { getFilePath } from '../utility/get_path.js';
+import { getErrors } from '../utility/errors.js';
+
 const read = async () => {
-    // Write your code here 
+    const fileName = getFilePath('files', 'fileToRead.txt', import.meta.url);
+
+    const error = getErrors('file');
+
+    if (!fs.existsSync(fileName)) {
+        throw error;
+    } else {
+        const readableStream = fs.createReadStream(fileName, {
+            encoding: 'utf8',
+        });
+        readableStream.on('data', (chunk) => {
+            process.stdout.write(chunk);
+        });
+        readableStream.on('error', function (err) {
+            console.log(err.stack);
+        });
+    }
 };
 
 await read();
